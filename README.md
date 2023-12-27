cb7effe497aeaebd8808a19be5a81b006e7d8a3c
Ariane-Sousa <ariane.crisousa@gmail.com>
2023-12-27 11:11:57 -0300

    header

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Django Web Framework e Django Rest Framework
 
 Repositorio destinado para os estudos de Django Web Framework e Django Rest Framework.
+
+# Comandos
+## Criar ambiente virtual
+python -m venv venv
+
+## Intalação do Django
+pip install Django
+
+## Iniciar servidor 
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
+
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
 
 def home(request):
-    return HttpResponse('Home')
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Ariane Sousa',
+    })
