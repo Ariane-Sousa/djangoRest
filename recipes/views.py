70106d9bfb6da7c87667c8bbd98b1566a08d0150
Ariane-Sousa <ariane.crisousa@gmail.com>
2023-12-28 13:15:32 -0300

    template, block

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
-
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Ariane Sousa',
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Ariane Sousa',
+    })
+
