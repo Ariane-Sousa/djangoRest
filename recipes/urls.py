93b3842beeb6e922eed29c257bb7e33e9a62d447
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-01-08 11:47:56 -0300

    admin, models recipes e category

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from recipes import views
+from . import views
+
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe)
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
